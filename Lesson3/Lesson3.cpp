d112b4635f7a01d174e02b8a052ff4cf34bc8ccd
Freed31 <abdulganiev_artem@mail.ru>
2024-07-09 19:40:06 +0200

    Ex3 is done

--- Lesson3/Lesson3.cpp
+++ Lesson3/Lesson3.cpp
@@ -1,9 +1,75 @@
 ﻿#include <iostream>
+#include <string>
 
+using namespace std;
 
+int main() {
+    setlocale(LC_CTYPE, "rus");
 
-int main()
-{
-    std::cout << "Hello World!\n";
+    string stop1 = "Улица программистов";
+    string stop2 = "Проспект алгоритмов";
+    string stop3 = "Бульвар разработчиков";
+    string stop4 = "Площадь кода";
+
+    int ticketPrice = 20;
+    int passengers = 0;
+    int totalEarnings = 0;
+
+    cout << "Прибываем на остановку \"" << stop1 << "\". В салоне пассажиров: " << passengers << endl;
+    int exited, entered;
+    cout << "Сколько пассажиров вышло на остановке? ";
+    cin >> exited;
+    cout << "Сколько пассажиров зашло на остановке? ";
+    cin >> entered;
+    passengers = passengers - exited + entered;
+    totalEarnings += entered * ticketPrice;
+    cout << "Отправляемся с остановки \"" << stop1 << "\". В салоне пассажиров: " << passengers << endl;
+    cout << "-----------Едем---------" << endl;
+
+    cout << "Прибываем на остановку \"" << stop2 << "\". В салоне пассажиров: " << passengers << endl;
+    cout << "Сколько пассажиров вышло на остановке? ";
+    cin >> exited;
+    cout << "Сколько пассажиров зашло на остановке? ";
+    cin >> entered;
+    passengers = passengers - exited + entered;
+    totalEarnings += entered * ticketPrice;
+    cout << "Отправляемся с остановки \"" << stop2 << "\". В салоне пассажиров: " << passengers << endl;
+    cout << "-----------Едем---------" << endl;
+
+    cout << "Прибываем на остановку \"" << stop3 << "\". В салоне пассажиров: " << passengers << endl;
+    cout << "Сколько пассажиров вышло на остановке? ";
+    cin >> exited;
+    cout << "Сколько пассажиров зашло на остановке? ";
+    cin >> entered;
+    passengers = passengers - exited + entered;
+    totalEarnings += entered * ticketPrice;
+    cout << "Отправляемся с остановки \"" << stop3 << "\". В салоне пассажиров: " << passengers << endl;
+    cout << "-----------Едем---------" << endl;
+
+    cout << "Прибываем на остановку \"" << stop4 << "\". В салоне пассажиров: " << passengers << endl;
+    cout << "Сколько пассажиров вышло на остановке? ";
+    cin >> exited;
+    cout << "Сколько пассажиров зашло на остановке? ";
+    cin >> entered;
+    passengers = passengers - exited + entered;
+    totalEarnings += entered * ticketPrice;
+    cout << "Отправляемся с остановки \"" << stop4 << "\". В салоне пассажиров: " << passengers << endl;
+
+    cout << "Всего заработали: " << totalEarnings << " руб." << endl;
+    double driverSalary = totalEarnings * 0.25;
+    double fuelCosts = totalEarnings * 0.20;
+    double taxes = totalEarnings * 0.20;
+    double maintenanceCosts = totalEarnings * 0.20;
+    double netIncome = totalEarnings - (driverSalary + fuelCosts + taxes + maintenanceCosts);
+
+    cout << "Зарплата водителя: " << driverSalary << " руб." << endl;
+    cout << "Расходы на топливо: " << fuelCosts << " руб." << endl;
+    cout << "Налоги: " << taxes << " руб." << endl;
+    cout << "Расходы на ремонт машины: " << maintenanceCosts << " руб." << endl;
+    cout << "Итого доход: " << netIncome << " руб." << endl;
+
+    return 0;
 }
+
+
 
---
+++ Lesson3_Ex3/Lesson3_Ex3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int beginHeight;
+    int dailyGrowth;
+    int nightFade;
+
+    cout << "Введите начальную высоту бамбука (в см): ";
+    cin >> beginHeight;
+    cout << "Введите ежедневный рост бамбука (в см): ";
+    cin >> dailyGrowth;
+    cout << "Введите количество бамбука, съедаемого гусеницами за одну ночь (в см): ";
+    cin >> nightFade;
+
+    int height = beginHeight + (dailyGrowth - nightFade) * 2 + dailyGrowth / 2;
+
+    cout << "Высота бамбука к середине третьего дня: " << height << " см" << std::endl;
+
+    return 0;
+}
